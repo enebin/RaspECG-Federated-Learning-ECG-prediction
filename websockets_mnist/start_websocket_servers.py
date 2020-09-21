b8683842a74f5fabd3c665accc6e8ea7530bad66
enebin <46271447+enebin@users.noreply.github.com>
2020-09-21 20:23:21 +0900

    m

--- websockets_mnist/start_websocket_servers.py
+++ websockets_mnist/start_websocket_servers.py
@@ -4,13 +4,14 @@ from pathlib import Path
 
 python = Path(sys.executable).name
 
-FILE_PATH = Path(__file__).resolve().parents[4].joinpath("run_websocket_server.py")
+FILE_PATH = Path(__file__).resolve().parent.joinpath("run_websocket_server.py")
+print(FILE_PATH)
 
 call_alice = [python, FILE_PATH, "--port", "8777", "--id", "alice"]
 
 call_bob = [python, FILE_PATH, "--port", "8778", "--id", "bob"]
 
-#call_charlie = [python, FILE_PATH, "--port", "8779", "--id", "charlie"]
+call_charlie = [python, FILE_PATH, "--port", "8779", "--id", "charlie"]
 
 
 print("Starting server for Alice")
@@ -19,5 +20,5 @@ subprocess.Popen(call_alice)
 print("Starting server for Bob")
 subprocess.Popen(call_bob)
 
-#print("Starting server for Charlie")
-#subprocess.Popen(call_charlie)
+print("Starting server for Charlie")
+subprocess.Popen(call_charlie)
